6c5cb16a498c770868700ba0d66ba626330c8bcf
ethan2813 <63579294+ethan2813@users.noreply.github.com>
2020-06-09 23:32:19 +0800

    2020-06-09 pm11:32 暫時完成

--- js/index.js
+++ js/index.js
@@ -4,15 +4,42 @@ const navSlide = () =>{
     const burger = document.querySelector('.burger');
     const nav_links = document.querySelector('.nav_links');
 
+    
+    
+    
     //漢堡條click事件
     burger.addEventListener('click',()=>{
+        
         // nav滑進來
         nav_links.classList.toggle('nav_active');
         //漢堡條變成x
         burger.classList.toggle('burger_active');
-
     })
 }
 
 // 執行navSlide函式
 navSlide();
+
+let lastScrollTop = 0;
+window.addEventListener('scroll',()=>{
+    var navagation = document.querySelector('.nav');
+    var nowScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    
+    if(nowScrollTop > lastScrollTop){
+        navagation.classList.add('fixed');
+    }else{
+        navagation.classList.remove('fixed');
+    }
+
+    lastScrollTop = ( nowScrollTop <= 0 ) ? 0 : nowScrollTop; // For Mobile or negative scrolling
+
+    console.log(nowScrollTop);
+    console.log(lastScrollTop);
+
+
+    
+}, false);
+
+
+
