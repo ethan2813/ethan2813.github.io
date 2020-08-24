f1db210519b1a0e15843fda8dc0e10cbda145557
ethan2813 <63579294+ethan2813@users.noreply.github.com>
2020-08-24 20:26:13 +0800

    上傳練習

--- ethan821/js/index.js
+++ ethan821/js/index.js
@@ -13,35 +13,36 @@
     setClock()
     setInterval(setClock,1000)
 
-    //  //------------------
-    //     //隨著滑鼠移動特效
-    //     //------------------
+        // //------------------
+        // //隨著滑鼠移動特效
+        // //------------------
         
-    //     var my_photo1 = $('.big_car')
+        // var my_photo1 = $('.back_car')
         
-    //     document.onmousemove = function (event) {
-    //         $.each(my_photo1, function (index, val) {
-    //             // console.log('index=', index);
-    //             var my_photo1_centerX = my_photo1[index].offsetLeft
-    //             my_photo1_centerX += 50
-    //             // console.log(my_photo1_centerX)
-    //             var my_photo1_centerY = my_photo1[index].offsetTop
-    //             my_photo1_centerY += 50
-    //             var offsetX;
-    //             var offsetY;
+        // document.onmousemove = function (event) {
+        //     $.each(my_photo1, function (index, val) {
+        //         // console.log('index=', index);
+        //         var my_photo1_centerX = my_photo1[index].offsetLeft;
+        //         // my_photo1_centerX += 200
+        //         // console.log(my_photo1_centerX)
+        //         var my_photo1_centerY = my_photo1[index].offsetTop;
+        //         // my_photo1_centerY += 42
+        //         var offsetX;
+        //         var offsetY;
+        //         // console.log(my_photo1_centerX)
 
 
-    //             let nowPosiotionX = event.pageX;
-    //             let nowPosiotionY = event.pageY;
-    //             // console.log("X " + nowPosiotionX + "Y" + nowPosiotionY);
+        //         let nowPosiotionX = event.pageX;
+        //         let nowPosiotionY = event.pageY;
+        //         // console.log("X " + nowPosiotionX + "Y" + nowPosiotionY);
 
 
-    //             // 鼠標的距離 = 現在數標位置 - 小圖的中心座標
-    //             offsetX = nowPosiotionX - my_photo1_centerX
-    //             offsetY = nowPosiotionY - my_photo1_centerY
-    //             my_photo1[index].style.transform = `translate(${offsetX /= 100}px , ${offsetY /= 100}px)`
+        //         // 鼠標的距離 = 現在數標位置 - 小圖的中心座標
+        //         offsetX = nowPosiotionX - my_photo1_centerX
+        //         offsetY = nowPosiotionY - my_photo1_centerY
+        //         my_photo1[index].style.transform = `translate(${offsetX /= 100}px , ${offsetY /= 100}px)`
 
 
-    //         })
-    //     };
+        //     })
+        // };
 })()
