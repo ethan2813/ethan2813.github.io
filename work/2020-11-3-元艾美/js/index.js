6beeaed5a69ae19f174132c65aa30d47d9784336
ethan2813 <eason2813@gmail.com>
2020-11-11 14:25:45 +0800

    update

--- work/2020-11-3-元艾美/js/index.js
+++ work/2020-11-3-元艾美/js/index.js
@@ -20,3 +20,31 @@ var swiper = new Swiper('.swiper-container', {
         }
     }
 });
+
+
+(function(){
+    // 漢堡條動畫 & nav_list滑出
+    $('.burger').on('click',(e)=>{
+        // console.log(e.currentTarget)
+        $(e.currentTarget).toggleClass('burger_active')
+        $('.nav_list').toggleClass('nav_active')
+
+    })
+
+
+    //nav 縮效果
+    let nav = document.querySelector('#nav .menu')
+    
+    $(window).scroll(()=>{
+        if($(this).scrollTop() > 10){
+            console.log('123')
+            nav.classList.add('move')
+        }else{
+            nav.classList.remove('move')
+        }
+    })
+
+    $('.nav_list').on('click','a',()=>{
+        $('.burger').click();
+    })
+})();
