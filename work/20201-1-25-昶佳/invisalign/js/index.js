49be8bd9d3d615ac9339f43d9774323be1fd273b
ethan2813 <eason2813@gmail.com>
2021-02-02 18:33:49 +0800

    update

---
+++ work/20201-1-25-昶佳/invisalign/js/index.js
@@ -0,0 +1,71 @@
+//漢堡條效果
+$('.burger').on('click',(e)=>{
+    $(e.currentTarget).toggleClass('burger_active')
+    $('.mobile_nav_list').toggleClass('nav_active')
+})
+
+//點mobile_nav_list會隱藏list
+$('.mobile_nav_list').on('click','a',()=>{
+    $('.burger').click();
+})
+
+//點空白地方關閉清單
+$(document).on('mouseup',(e)=>{
+    if($('.mobile_nav_list').hasClass('nav_active')){
+        if(!$(e.target).closest('.nav_active,.burger').length){
+            $('.mobile_nav_list').toggleClass('nav_active');
+            $('.burger').toggleClass('burger_active')
+        }
+    }
+})
+
+
+//totop
+$('#totop').on('click', () => {
+    $('html,body').animate({ scrollTop: 0 }, 300);
+})
+
+
+//totop隱藏效果
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 400) {
+        $('#totop').fadeIn(200);
+    } else {
+        $('#totop').stop().fadeOut(200)
+    }
+    // console.log($(this).scrollTop())
+})
+
+//nav 縮放
+let nav = document.querySelector('#nav')
+let logo_photo = document.querySelector('.logo_photo')
+$(window).scroll(() => {
+    if($(this).scrollTop() > 10){
+        $('#nav').addClass('move')
+    }else{
+        $('#nav').removeClass('move')
+    }
+})
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    speed: 2000,
+    autoplay: {
+        delay: 5000,
+    },
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true,
+    // },
+    //RWD(以上)
+    // breakpoints:{
+    //     800:{
+    //         slidesPerView: 3,
+    //     },
+    //     450:{
+    //         slidesPerView: 2,
+    //     }
+    // }
+});
