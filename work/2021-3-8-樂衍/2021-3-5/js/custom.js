364f1cefc25e22da732f47a386b31f3db6394c1f
ethan2813 <eason2813@gmail.com>
2021-03-08 17:41:41 +0800

    update

---
+++ work/2021-3-8-樂衍/2021-3-5/js/custom.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+	//導覽列滾動
+	$(window).on("scroll",function(){
+		var scrollDistance = $(window).scrollTop();
+		var $header = $(".js-header");
+		if(scrollDistance > 80){
+			$header.addClass("header--scrolling");
+		}
+		else{
+			$header.removeClass("header--scrolling");
+		}
+	})
+	$(".dropdown-toggle").hover(function(){
+		// $(".dropdown-menu").slideToggle("300");
+		$(".dropdown-menu").css("display","block");
+	});
+
+	$('.webicon-inner').owlCarousel({
+	    loop:true,
+	    margin:0,
+	    nav:true,
+	    navText:[],
+	    dots: false,
+	    autoplay:true,
+	    smartSpeed:400,
+	    responsive:{
+	        0:{
+	            items:4
+	        },
+	        768:{
+	            items:6
+	        },
+	        1600:{
+	            items:8
+	        }
+	    }
+	})
+
+	$('.line-inner').owlCarousel({
+	    loop:true,
+	    margin:0,
+	    nav:true,
+	    navText:[],
+	    dots:true,
+	    autoplay:true,
+	    smartSpeed:1000,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:1
+	        },
+	        768:{
+	            items:1
+	        }
+	    }
+	})
+});
+
+
+
+//類似fullpage效果
+$(document).on('click','a',function(evt){
+	var w=$(window).width();
+	var target= $(this).attr("href");
+	if(w >= 961){
+		$('html,body').animate({
+	    	scrollTop: $(target).offset().top
+		},800);
+	}
+});
+
+
+$(window).load(function() {
+    $('#before_after_images').twentytwenty();
+});
---
+++ work/2021-3-8-樂衍/2021-3-5/js/marketing.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper('.swiper-container', {
+    observer: true,
+    observeParents: true,
+    slidesPerView: 8,
+    spaceBetween: 15,
+    loop: true,
+    speed: 300,
+    autoplay: {
+        delay: 3000,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+       
+    },
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true,
+    // },
+    //RWD(以上)
+    breakpoints:{
+        1500:{
+            slidesPerView: 7,
+            spaceBetween: 50,
+            
+        },
+        800:{
+            slidesPerView: 5,
+            spaceBetween: 30,
+
+        },
+        0:{
+            slidesPerView: 3,
+            spaceBetween: 15,
+
+        }
+    }
+});
