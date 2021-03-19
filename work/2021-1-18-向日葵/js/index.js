5dfb2aa78207f11af49ea802e133fb1d9ca3d936
ethan2813 <eason2813@gmail.com>
2021-03-19 15:22:47 +0800

    update

--- work/2021-1-18-向日葵/js/index.js
+++ work/2021-1-18-向日葵/js/index.js
@@ -47,16 +47,24 @@ $(document).ready(function(){
     window.addEventListener('scroll',function(){
         let this_scroll = this.scrollY; //下移量
         var topHeight = $(window).height(); //畫面總高度
+        let top = $(this).scrollTop() / 15; //位移倍率
 
         //關於本院標題
         let about_title = $('.about .title_')
-        let about_title_scroll = about_title.offset().top - topHeight;  //關於本院標題下移量
+        //關於本院標題下移量
+        let about_title_scroll = about_title.offset().top - topHeight;  
 
-        //關於本院內文
-        let about_content = $('main#main .about .content_ .article .content')
-        let about_content_scroll = about_content.offset().top - topHeight;//關於本院內文下移量
+        //關於本院內文標題
+        let about_content_h3 = $('main#main .about .content_ .article .content h3')
+        //關於本院內文標題下移量
+        let about_content_h3_scroll = about_content_h3.offset().top - topHeight;
 
-        let top = $(this).scrollTop() / 25; //位移倍率
+        //關於本院內文內文
+        let about_content_p = $('main#main .about .content_ .article .content p')
+        //關於本院內文內文下移量
+        let about_content_p_scroll = about_content_h3.offset().top - topHeight;
+
+        
         
         // 關於本院 標題
         if(this_scroll > about_title_scroll){
@@ -64,10 +72,16 @@ $(document).ready(function(){
             about_title.css('opacity','1')
         }
 
-        // 關於本院 文字
-        if(this_scroll > about_content_scroll){
-            about_content.css('transform',`translate(0% , -${top}px)`)
-            about_content.css('opacity','1')
+        // 關於本院 文字標題
+        if(this_scroll > about_content_h3_scroll){
+            about_content_h3.css('transform',`translate(0% , -${top}px)`)
+            about_content_h3.css('opacity','1')
+        }
+
+        // 關於本院 文字內文
+        if(this_scroll > about_content_p_scroll){
+            about_content_p.css('transform',`translate(0% , -${top}px)`)
+            about_content_p_scroll.css('opacity','1')
         }
     })
 })
