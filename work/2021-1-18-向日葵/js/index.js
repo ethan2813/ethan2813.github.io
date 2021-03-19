8592286af431545f1a3c9fdfa4753c5519d9d3d2
ethan2813 <eason2813@gmail.com>
2021-03-19 13:53:32 +0800

    update

--- work/2021-1-18-向日葵/js/index.js
+++ work/2021-1-18-向日葵/js/index.js
@@ -2,19 +2,40 @@
 $('.burger').on('click',(e)=>{
     $('.nav_list').addClass('active')
 })
-
 $('.nav_list .close').on('click',(e)=>{
     $('.nav_list').removeClass('active')
 })
 
 //電話隱藏效果
 $(window).scroll(function () {
-    
     if ($(this).scrollTop() > 40) {
         $('.phone_number').fadeOut(200);
     } else {
         $('.phone_number').stop().fadeIn(200)
     }
-    // console.log($(this).scrollTop())
 })
+
+//點空白地方關閉清單
+$(document).on('mouseup',(e)=>{
+    if($('.nav_list').hasClass('active')){
+        if(!$(e.target).closest('.nav_list li a,.close').length){
+            $('.nav_list').toggleClass('active');
+        }
+    }
+})
+
+//totop
+$('#totop').on('click',()=>{
+    $('html,body').animate({scrollTop:0},300);
+})
+
+//totop隱藏效果
+$(window).scroll(function(){
+    if($(this).scrollTop() > 400){
+        console.log($(this).scrollTop())
+        $('#totop').fadeIn(200);
+    }else{
+        $('#totop').stop().fadeOut(200)
+    }
+}).scroll();
 
