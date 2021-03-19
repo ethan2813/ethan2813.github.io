62ed5a8c334b35feeb4dd76b95d34aca018d6fef
ethan2813 <eason2813@gmail.com>
2021-03-19 15:07:46 +0800

    update

--- work/2021-1-18-向日葵/js/index.js
+++ work/2021-1-18-向日葵/js/index.js
@@ -37,5 +37,38 @@ $(window).scroll(function(){
     }else{
         $('#totop').stop().fadeOut(200)
     }
+    
 }).scroll();
+
+
+// scroll 效果
+$(document).ready(function(){
+    
+    window.addEventListener('scroll',function(){
+        let this_scroll = this.scrollY; //下移量
+        var topHeight = $(window).height(); //畫面總高度
+
+        //關於本院標題
+        let about_title = $('.about .title_')
+        let about_title_scroll = about_title.offset().top - topHeight;  //關於本院標題下移量
+
+        //關於本院內文
+        let about_content = $('main#main .about .content_ .article .content')
+        let about_content_scroll = about_content.offset().top - topHeight;//關於本院內文下移量
+
+        let top = $(this).scrollTop() / 25; //位移倍率
+        
+        // 關於本院 標題
+        if(this_scroll > about_title_scroll){
+            about_title.css('transform',`translate(0% , -${top}px)`)
+            about_title.css('opacity','1')
+        }
+
+        // 關於本院 文字
+        if(this_scroll > about_content_scroll){
+            about_content.css('transform',`translate(0% , -${top}px)`)
+            about_content.css('opacity','1')
+        }
+    })
+})
 
