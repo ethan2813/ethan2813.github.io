bc6dc0bb875fb0a2a2d7cee39b92463fca8e67c6
ethan2813 <eason2813@gmail.com>
2021-08-05 13:56:47 +0800

    update

---
+++ work/2021-8-7 峰/js/index.js
@@ -0,0 +1,15 @@
+// 漢堡條效果
+$('.burger').on('click', (e) => {
+    $(e.currentTarget).toggleClass('burger_active')
+    $('.mobile_nav_list').toggleClass('nav_active')
+})
+
+// 點擊漢堡條 跳出選單
+$('.burger').on('click',()=>{
+    $('.mobile_right').toggleClass('active')
+})
+
+new fullpage('#fullpage', {
+    sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+  });   
+
