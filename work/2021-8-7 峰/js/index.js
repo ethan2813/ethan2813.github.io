379f9131f1cded6ea4e9e2275282b84c3c1b08bc
ethan2813 <eason2813@gmail.com>
2021-08-05 14:44:22 +0800

    update

--- work/2021-8-7 峰/js/index.js
+++ work/2021-8-7 峰/js/index.js
@@ -13,16 +13,4 @@ new fullpage('#fullpage', {
     sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
 });
 
-var myFullpage = new fullpage('#fullpage', {
-    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-    anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
-    menu: '#menu',
-    continuousVertical: true,
-    afterLoad: function (anchorLink, index) {
-        console.log("AFTER LOAD - anchorLink:" + anchorLink + " index:" + index);
-    },
-    onLeave: function (index, nextIndex, direction) {
-        console.log("ONLEAVE - index:" + index + " nextIndex:" + nextIndex + " direction:" + direction);
 
-    },
-});
