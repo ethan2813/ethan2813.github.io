b39ff64e89e9ba7b9e58af863e8bb201544dd6ff
ethan2813 <eason2813@gmail.com>
2021-10-04 17:53:40 +0800

    update

--- work/2021-8-31-danbara/js/index.js
+++ work/2021-8-31-danbara/js/index.js
@@ -13,4 +13,36 @@ new fullpage('#fullpage', {
     sectionsColor: [],
 });
 
+// 點空白地方關閉選單
+// $(document).on('mouseup',(e)=>{
+//     if($('.mobile_nav_list').hasClass('active')){
+//          if (!$(e.target).closest('.content_').length) {
+//              $('.mobile_nav_list').removeClass('active')
+//              $('.burger').removeClass('burger_active')
+//          }
+//     }
+// })
+
+$(document).on('mouseup',(e)=>{
+    if($('.mobile_right').hasClass('active')){
+        if(!$(e.target).closest('.mobile_right').length){
+            $('.mobile_right').removeClass('active')
+            $('.burger').removeClass('burger_active')
+
+        }
+    }
+})
+
+let A = document.querySelector('#test #A_')
+let B = document.querySelector('#test #B_')
+let text = document.querySelector('#test ')
+
+
+
+function handler (e){
+    console.log(e.target)
+    console.log(e.currentTarget)
+}
+
+A.addEventListener('click',handler)
 
