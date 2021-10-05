f5c00a47d120e557c79db2530a0a910bc7162da8
ethan2813 <eason2813@gmail.com>
2021-10-05 14:38:42 +0800

    update

--- work/2021-8-31-danbara/js/index.js
+++ work/2021-8-31-danbara/js/index.js
@@ -14,18 +14,9 @@ new fullpage('#fullpage', {
 });
 
 // 點空白地方關閉選單
-// $(document).on('mouseup',(e)=>{
-//     if($('.mobile_nav_list').hasClass('active')){
-//          if (!$(e.target).closest('.content_').length) {
-//              $('.mobile_nav_list').removeClass('active')
-//              $('.burger').removeClass('burger_active')
-//          }
-//     }
-// })
-
 $(document).on('mouseup',(e)=>{
     if($('.mobile_right').hasClass('active')){
-        if(!$(e.target).closest('.mobile_right').length){
+        if(!$(e.target).closest('.mobile_right,.burger').length){
             $('.mobile_right').removeClass('active')
             $('.burger').removeClass('burger_active')
 
@@ -33,16 +24,43 @@ $(document).on('mouseup',(e)=>{
     }
 })
 
-let A = document.querySelector('#test #A_')
-let B = document.querySelector('#test #B_')
-let text = document.querySelector('#test ')
+// gototop
+$('.gototop').on('click',(e)=>{
+    $('html,body').animate({scrollTop:0},300);
+    // console.log(e.currentTarget)
+})
+
+// $(window).scroll((e)=>{
+//     if($(e.target).scrollTop() > 400){
+//         $('.gototop').fadeIn(200)
+//     }else{
+//         $('.gototop').stop().fadeout(200)
+//     }
+// })
+
+$('.gototop').stop().fadeOut(0)
+$(window).scroll(function () {
+    
+    if ($(this).scrollTop() > 400) {
+        $('.gototop').fadeIn(200);
+    } else {
+        $('.gototop').stop().fadeOut(200)
+    }
+    // console.log($(this).scrollTop())
+})
 
 
 
-function handler (e){
-    console.log(e.target)
-    console.log(e.currentTarget)
-}
+// let A = document.querySelector('#test #A_')
+// let B = document.querySelector('#test #B_')
+// let text = document.querySelector('#test ')
 
-A.addEventListener('click',handler)
+
+
+// function handler (e){
+//     console.log(e.target)
+//     console.log(e.currentTarget)
+// }
+
+// A.addEventListener('click',handler)
 
